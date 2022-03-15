a6956626bca85714ff73bc6e1f0be2e9a50059e4
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-03-15 20:26:58 +0800

    feat(data_process): create data process function, move the old pretrained dataset

---
+++ data_process.py
@@ -0,0 +1,42 @@
+import json
+import random
+
+import spacy
+from spacy.tokens import Span
+
+from src.utils.spacy import char_idx_to_word_idx
+
+INPUT = "all.jsonl"
+OUTPUT1 = "train.txt"
+OUTPUT2 = "dev.txt"
+PROPORTION = 80
+
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_lg")
+    fp1 = open(OUTPUT1, "w")
+    fp2 = open(OUTPUT2, "w")
+
+    with open(INPUT, "r") as file:
+        for idx, line in enumerate(file):
+            dice = random.randint(1, 100)
+            if dice > PROPORTION:
+                fp = fp2
+            else:
+                fp = fp1
+
+            a = json.loads(line)
+            text = a["text"]
+            sent = nlp(text)
+            anno = list()
+            for label in a["labels"]:
+                s, e, lab = label
+                start, end = char_idx_to_word_idx(sent[:], s, e)
+                anno.append(Span(sent, start, end, lab))
+            sent.set_ents(anno)
+            for tok in sent:
+                if tok.ent_type_ == "":
+                    rep = f"{tok.text} O"
+                else:
+                    rep = f"{tok.text} {tok.ent_iob_}-{tok.ent_type_}"
+                print(rep, file=fp)
+            print(file=fp)
